1dc310a46c888af68e0f60132b703a859987c101
kylee <kylee@devband.com>
2018-04-25 13:15:41 +0900

    fix exception

--- app/src/main/java/com/devband/tronwalletforandroid/tron/exception/InvalidPasswordException.java
+++ app/src/main/java/com/devband/tronwalletforandroid/tron/exception/InvalidPasswordException.java
@@ -16,8 +16,4 @@ public class InvalidPasswordException extends RuntimeException {
     public InvalidPasswordException(Throwable cause) {
         super(cause);
     }
-
-    public InvalidPasswordException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
