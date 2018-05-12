21ad3b27249dbee4b97792dee302e6732277f052
kylee <kylee@devband.com>
2018-05-12 23:43:37 +0900

    remove old activity

--- app/src/main/java/com/devband/tronwalletforandroid/ui/importwallet/ImportWalletView.java
+++
@@ -1,9 +0,0 @@
-package com.devband.tronwalletforandroid.ui.importwallet;
-
-import android.support.annotation.Nullable;
-
-import com.devband.tronwalletforandroid.ui.mvp.IView;
-
-public interface ImportWalletView extends IView {
-
-}
