15f60e26113d8301082eda6a61e9bc97e59fe893
dhs <hs@dhsui-MacBook-Pro.local>
2018-05-29 21:01:58 +0900

    connect block list to account

--- app/src/main/java/com/devband/tronwalletforandroid/ui/blockexplorer/adapter/BlockAdapter.java
+++ app/src/main/java/com/devband/tronwalletforandroid/ui/blockexplorer/adapter/BlockAdapter.java
@@ -1,7 +1,10 @@
 package com.devband.tronwalletforandroid.ui.blockexplorer.adapter;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +14,7 @@ import com.devband.tronlib.dto.Block;
 import com.devband.tronlib.dto.Blocks;
 import com.devband.tronwalletforandroid.R;
 import com.devband.tronwalletforandroid.common.Constants;
+import com.devband.tronwalletforandroid.ui.accountdetail.AccountDetailActivity;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -46,11 +50,22 @@ public class BlockAdapter extends RecyclerView.Adapter<BlockAdapter.BlockViewHol
 
         holder.txtNumber.setText("#" + Constants.numberFormat.format(block.getNumber()));
         holder.txtTransaction.setText(String.valueOf(block.getNrOfTrx()));
-        holder.txtProducedBy.setText(block.getWitnessAddress());
+
+        SpannableString content = new SpannableString(block.getWitnessAddress());
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+
+        holder.txtProducedBy.setText(content);
+        holder.txtProducedBy.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), AccountDetailActivity.class);
+            intent.putExtra(AccountDetailActivity.EXTRA_ADDRESS, block.getWitnessAddress());
+            view.getContext().startActivity(intent);
+        });
 
         Date date = new Date(block.getTimestamp());
         holder.txtTimestamp.setText(Constants.sdf.format(date));
     }
+
+
 
     @Override
     public int getItemCount() {
