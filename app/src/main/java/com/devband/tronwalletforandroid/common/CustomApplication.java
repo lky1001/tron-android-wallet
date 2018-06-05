3b0371046adced4408b0cc7a324d524017dfb162
kylee <kylee@devband.com>
2018-06-05 11:31:37 +0900

    Add Crashlytics

--- app/src/main/java/com/devband/tronwalletforandroid/common/CustomApplication.java
+++ app/src/main/java/com/devband/tronwalletforandroid/common/CustomApplication.java
@@ -4,7 +4,17 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class CustomApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
 
     @Override
     protected void attachBaseContext(Context base) {
