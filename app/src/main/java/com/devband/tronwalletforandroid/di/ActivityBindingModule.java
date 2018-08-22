b05d2e5d0a39a41e35f0ff98eefe2ba279ae5650
kylee <kylee@devband.com>
2018-08-22 13:29:25 +0900

    login activity with dagger

--- app/src/main/java/com/devband/tronwalletforandroid/di/ActivityBindingModule.java
+++ app/src/main/java/com/devband/tronwalletforandroid/di/ActivityBindingModule.java
@@ -2,6 +2,8 @@ package com.devband.tronwalletforandroid.di;
 
 import com.devband.tronwalletforandroid.ui.intro.IntroActivity;
 import com.devband.tronwalletforandroid.ui.intro.IntroActivityModule;
+import com.devband.tronwalletforandroid.ui.login.LoginActivity;
+import com.devband.tronwalletforandroid.ui.login.LoginActivityModule;
 
 import dagger.Module;
 import dagger.android.AndroidInjectionModule;
@@ -13,4 +15,8 @@ public abstract class ActivityBindingModule {
     @ActivityScoped
     @ContributesAndroidInjector(modules = { IntroActivityModule.class })
     abstract IntroActivity bindIntroActivity();
+
+    @ActivityScoped
+    @ContributesAndroidInjector(modules = { LoginActivityModule.class })
+    abstract LoginActivity bindLoginActivity();
 }
