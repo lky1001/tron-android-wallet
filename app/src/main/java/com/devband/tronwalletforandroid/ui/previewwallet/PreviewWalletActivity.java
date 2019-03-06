f05a2ef1001fa1bc2cf2067757ccb65c50285c7e
lky1001 <lky1001@gmail.com>
2019-03-06 22:47:34 +0900

    preview wallet ui

--- app/src/main/java/com/devband/tronwalletforandroid/ui/previewwallet/PreviewWalletActivity.java
+++ app/src/main/java/com/devband/tronwalletforandroid/ui/previewwallet/PreviewWalletActivity.java
@@ -2,13 +2,31 @@ package com.devband.tronwalletforandroid.ui.previewwallet;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.Toolbar;
+
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import com.devband.tronwalletforandroid.R;
 import com.devband.tronwalletforandroid.common.CommonActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PreviewWalletActivity extends CommonActivity implements PreviewWalletView {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView mRecyclerView;
+
+    @BindView(R.id.swipe_layout)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
