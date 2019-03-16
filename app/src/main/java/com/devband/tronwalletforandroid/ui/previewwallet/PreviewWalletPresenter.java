5b8da0301d6755255cd0b45c190386bde6250d27
kylee <kylee@devband.com>
2019-03-16 20:23:02 +0900

    get account list

--- app/src/main/java/com/devband/tronwalletforandroid/ui/previewwallet/PreviewWalletPresenter.java
+++ app/src/main/java/com/devband/tronwalletforandroid/ui/previewwallet/PreviewWalletPresenter.java
@@ -1,15 +1,21 @@
 package com.devband.tronwalletforandroid.ui.previewwallet;
 
 import com.devband.tronlib.TronNetwork;
+import com.devband.tronwalletforandroid.common.AdapterDataModel;
+import com.devband.tronwalletforandroid.database.model.AccountModel;
 import com.devband.tronwalletforandroid.rxjava.RxJavaSchedulers;
 import com.devband.tronwalletforandroid.tron.Tron;
 import com.devband.tronwalletforandroid.ui.mvp.BasePresenter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PreviewWalletPresenter extends BasePresenter<PreviewWalletView> {
 
     private Tron mTron;
     private TronNetwork mTronNetwork;
     private RxJavaSchedulers mRxJavaSchedulers;
+    private AdapterDataModel<TronWallet> mAdapterDataModel;
 
     public PreviewWalletPresenter(PreviewWalletView view, Tron tron, TronNetwork tronNetwork,
             RxJavaSchedulers rxJavaSchedulers) {
@@ -19,9 +25,30 @@ public class PreviewWalletPresenter extends BasePresenter<PreviewWalletView> {
         this.mRxJavaSchedulers = rxJavaSchedulers;
     }
 
+    public void setAdapterDataModel(AdapterDataModel<TronWallet> adapterDataModel) {
+        this.mAdapterDataModel = adapterDataModel;
+    }
+
     @Override
     public void onCreate() {
+        mTron.getAccountList()
+                .subscribeOn(mRxJavaSchedulers.getIo())
+                .observeOn(mRxJavaSchedulers.getMainThread())
+                .map(accountList -> {
+                    List<TronWallet> walletList = new ArrayList<>();
+                    for (AccountModel accountModel : accountList) {
+                        walletList.add(TronWallet.builder()
+                                .accountName(accountModel.getName())
+                                .address(accountModel.getAddress())
+                                .build());
+                    }
 
+                    return walletList;
+                })
+                .subscribe(accountList -> {
+                    mAdapterDataModel.clear();
+                    mAdapterDataModel.addAll(accountList);
+                }, e -> e.printStackTrace());
     }
 
     @Override
--- app/src/main/java/com/devband/tronwalletforandroid/ui/previewwallet/TronWallet.java
+++ app/src/main/java/com/devband/tronwalletforandroid/ui/previewwallet/TronWallet.java
@@ -1,4 +1,19 @@
 package com.devband.tronwalletforandroid.ui.previewwallet;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TronWallet {
+
+    private String accountName;
+    private String address;
+    private double balance;
 }
