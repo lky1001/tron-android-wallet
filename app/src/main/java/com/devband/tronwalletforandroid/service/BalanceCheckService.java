ba955a4392ab36300a95b1c60511387773989828
kylee <kylee@devband.com>
2019-04-02 22:55:50 +0900

    service

--- app/src/main/java/com/devband/tronwalletforandroid/service/BalanceCheckService.java
+++ app/src/main/java/com/devband/tronwalletforandroid/service/BalanceCheckService.java
@@ -1,11 +1,36 @@
 package com.devband.tronwalletforandroid.service;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class BalanceCheckService extends Service {
+
+    private static final String CHANNEL_ID = "balance_check_service_channel";
+
+    @MainThread
+    public static void startService(@NonNull Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(new Intent(context, BalanceCheckService.class));
+        } else {
+            context.startService(new Intent(context, BalanceCheckService.class));
+        }
+    }
+
+    @MainThread
+    public static void stopService(@NonNull Context context) {
+        context.stopService(new Intent(context, BalanceCheckService.class));
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return Service.START_STICKY;
+    }
 
     @Nullable
     @Override
