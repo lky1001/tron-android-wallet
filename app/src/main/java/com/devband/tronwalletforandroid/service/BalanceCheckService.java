2a62f0544f127672d8acb5767a26b42dfe4d315f
kylee <kylee@devband.com>
2019-04-05 00:18:06 +0900

    notification

--- app/src/main/java/com/devband/tronwalletforandroid/service/BalanceCheckService.java
+++ app/src/main/java/com/devband/tronwalletforandroid/service/BalanceCheckService.java
@@ -1,5 +1,8 @@
 package com.devband.tronwalletforandroid.service;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -8,12 +11,29 @@ import android.os.IBinder;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+
+import com.devband.tronwalletforandroid.ui.login.LoginActivity;
 
 public class BalanceCheckService extends Service {
 
+    private static final String CHANNEL_ID = "tron_balance_check_channel";
+
     @MainThread
     public static void startService(@NonNull Context context) {
+        Intent notificationIntent = new Intent(context, LoginActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
+
+        NotificationCompat.Builder builder;
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Tron Wallet",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+
+            ((NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE))
+                    .createNotificationChannel(channel);
+
             context.startForegroundService(new Intent(context, BalanceCheckService.class));
         } else {
             context.startService(new Intent(context, BalanceCheckService.class));
