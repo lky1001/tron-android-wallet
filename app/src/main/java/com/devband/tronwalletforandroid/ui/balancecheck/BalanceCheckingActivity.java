de349a05d5afe886904faafe941af7cf47d29fe8
kylee <kylee@devband.com>
2019-04-09 23:43:35 +0900

    bind view

--- app/src/main/java/com/devband/tronwalletforandroid/ui/balancecheck/BalanceCheckingActivity.java
+++ app/src/main/java/com/devband/tronwalletforandroid/ui/balancecheck/BalanceCheckingActivity.java
@@ -2,13 +2,28 @@ package com.devband.tronwalletforandroid.ui.balancecheck;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.RecyclerView;
 
+import com.devband.tronwalletforandroid.R;
 import com.devband.tronwalletforandroid.common.CommonActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class BalanceCheckingActivity extends CommonActivity {
+
+    @BindView(R.id.check_listview)
+    public RecyclerView recyclerView;
+
+    @BindView(R.id.fab_add)
+    public FloatingActionButton addButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_balance_checking);
+
+        ButterKnife.bind(this);
     }
 }
